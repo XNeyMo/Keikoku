6505520ea3347f1c0b87be904f5d76a88b524f8f
Mathew <mathewpro123@gmail.com>
2023-12-25 22:35:53 -0500

    login button with google

--- src/components/LogInButton.jsx
+++ src/components/LogInButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LogInButton = () => {
   return (
-    <button className="bg-lightcolor text-darkcolor font-bold py-4 px-12 text-2xl rounded-[20px] max-w-64">
+    <button onclick="window.location.href = redirect('/login/google')" className="bg-lightcolor text-darkcolor font-bold py-4 px-12 text-2xl rounded-[20px] max-w-64">
       Log In
     </button>
   )
