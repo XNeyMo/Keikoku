507e9b9ec14f77004d27aff7155eed5c1df14ac2
Mathew <mathewpro123@gmail.com>
2023-12-26 16:41:44 -0500

    ola again

--- src/Backend/db/firebase_config/firebase.js
+++ src/Backend/db/firebase_config/firebase.js
@@ -21,9 +21,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
-export const auth = getAuth();
+export const auth = getAuth(app);
+auth.languageCode = 'en';
+
 export const googleprovider = new GoogleAuthProvider();
-
 export const db = getFirestore(app);
