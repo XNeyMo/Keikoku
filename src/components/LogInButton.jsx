cfb0bb7a1f9040d24e4cf42f903db42c26dd13de
XNeyMo <xneymodev@gmail.com>
2023-12-26 16:43:55 -0500

    si

--- src/components/LogInButton.jsx
+++ src/components/LogInButton.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 
 const LogInButton = () => {
   return (
-    <a href='/dashboard' className="bg-lightcolor text-darkcolor font-bold py-4 px-12 text-2xl rounded-[20px] max-w-64 text-center">
+    <a href='/' className="bg-lightcolor text-darkcolor font-bold py-4 px-12 text-2xl rounded-[20px] max-w-64 text-center">
       Log In
     </a>
   )
