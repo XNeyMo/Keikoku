5c2c70a399d54c8ffb649cd3ffb4040505e3d6f0
Mathew <mathewpro123@gmail.com>
2023-12-25 22:58:36 -0500

    a

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,3 @@
 /// <reference types="astro/client" />
+export const GOOGLE_CLIENT_ID = '21944539281-9k0i9koj5ieibjnuf190sihv85b5bnb0.apps.googleusercontent.com';
+export const GOOGLE_CLIENT_SECRET = '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';
